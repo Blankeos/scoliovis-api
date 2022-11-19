29dc99b3ecd6c1fd6624cbebc3ba769d05680548
Blankeos <carloantonioct@gmail.com>
2022-11-19 15:27:09 +0800

    Deleted unnecessary files and implement kkprcnn + cobb angle cal

--- scoliovis/get_model.py
+++ scoliovis/get_model.py
@@ -1,60 +1,76 @@
 import os
-import torch
 from pathlib import Path
-import tensorflow as tf
-    
-def _download_model():
-    print("DETA: Downloading Landmark Estimation Model...")
+
+# Keypoint RCNN Model
+import torch
+from torchvision.models.detection.rpn import AnchorGenerator
+import torchvision
+
+def _download_kprcnn_model():
+    print("DETA: Downloading Keypoint RCNN Model...")
     from deta import Deta
     deta = Deta(os.environ.get("DETA_ID"))
     models = deta.Drive("models")
-    model_file = models.get('scoliovis_segmentation_model.h5')
-    with open("models/scoliovis_segmentation_model.h5", "wb+") as f:
+    model_file = models.get('keypointsrcnn_weights.pt')
+    with open("models/keypointsrcnn_weights.pt", "wb+") as f:
         for chunk in model_file.iter_chunks(1024):
             f.write(chunk)
-        print("DETA: Landmark Estimation Model downloaded.")
+        print("DETA: Keypoint RCNN model downloaded.")
         model_file.close()
 
-def _download_detection_model():
-    print("DETA: Downloading Object Detection Model...")
-    from deta import Deta
-    deta = Deta(os.environ.get("DETA_ID"))
-    models = deta.Drive("models")
-    model_file = models.get('detection_model.pt')
-    with open("models/detection_model.pt", "wb+") as f:
-        for chunk in model_file.iter_chunks(1024):
-            f.write(chunk)
-        print("DETA: Object Detection model downloaded.")
-        model_file.close()
 
-def get_detection_model():
+def get_kprcnn_model():
     model_folder = Path("models")
     if not model_folder.exists():
         os.mkdir("models")
-    model_path = Path("models/detection_model.pt")
-
+    model_path = Path("models/keypointsrcnn_weights.pt")
+    
     # Download if the model does not exist
     if model_path.is_file():
-        print("Detection Model is already downloaded.")
+        print("Keypoint RCNN Model is already downloaded.")
     else:
-        print("Detection Model was NOT FOUND.")
-        _download_detection_model()
-    
-    # Get model from path and return
-    model = torch.hub.load('./yolov5', 'custom', path='./models/detection_model.pt', source='local')
-    return model
-    
-def get_model():
-    model_folder = Path("models")
-    if not model_folder.exists():
-        os.mkdir("models")
-    model_path = Path('models/scoliovis_segmentation_model.h5')
-    if model_path.is_file():
-        print("The model is already downloaded.")
-    else:
-        print("The model is NOT downloaded yet.")
-        _download_model()
-    
-    model = tf.keras.models.load_model('models/scoliovis_segmentation_model.h5')
-    return model
+        print("Keypoint RCNN Model was NOT FOUND.")
+        _download_kprcnn_model()
+
+    num_keypoints = 4
+    anchor_generator = AnchorGenerator(sizes=(32, 64, 128, 256, 512), aspect_ratios=(0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0))
+    model = torchvision.models.detection.keypointrcnn_resnet50_fpn(pretrained=False,
+                                                                   pretrained_backbone=True,
+                                                                   num_keypoints=num_keypoints,
+                                                                   num_classes = 2, # Background is the first class, object is the second class
+                                                                   rpn_anchor_generator=anchor_generator)
+    if model_path:
+        state_dict = torch.load(model_path, map_location=torch.device('cpu'))
+        model.load_state_dict(state_dict)        
         
+    return model
+
+# YoloV5 Model
+# def _download_detection_model():
+#     print("DETA: Downloading Object Detection Model...")
+#     from deta import Deta
+#     deta = Deta(os.environ.get("DETA_ID"))
+#     models = deta.Drive("models")
+#     model_file = models.get('detection_model.pt')
+#     with open("models/detection_model.pt", "wb+") as f:
+#         for chunk in model_file.iter_chunks(1024):
+#             f.write(chunk)
+#         print("DETA: Object Detection model downloaded.")
+#         model_file.close()
+
+# def get_detection_model():
+#     model_folder = Path("models")
+#     if not model_folder.exists():
+#         os.mkdir("models")
+#     model_path = Path("models/detection_model.pt")
+
+#     # Download if the model does not exist
+#     if model_path.is_file():
+#         print("Detection Model is already downloaded.")
+#     else:
+#         print("Detection Model was NOT FOUND.")
+#         _download_detection_model()
+    
+#     # Get model from path and return
+#     model = torch.hub.load('./yolov5', 'custom', path='./models/detection_model.pt', source='local')
+#     return model
